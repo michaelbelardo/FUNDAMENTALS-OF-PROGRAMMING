ef57276102efe4609f254bb52a5da94596bdfe8a
michaelbelardo <2025-1-00874@lpu.edu.ph>
2025-09-30 11:24:15 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,9 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-   float num1, num2;
-   
-   printf("enter number:");
-   scanf("%f" , &num1);
-   
-   printf("enter number:");
-   scanf("%f" , &num2);
-   
-   printf("\n--- calculations ---\n");
-   printf("addition: %.2f\n" , num1 + num2);
-   printf("subtraction: %.2f\n" , num1 - num2); 
-   printf("multiplication: %.2f\n" , num1 * num2);
-   printf("division: %2f\n" , num1 / num2);
-   printf("remainder: %d\n" , (int)num1 % (int)num2);
-   printf
-   ("\n------THANK YOU MS.ROMA------\n");
+    for (int i = 1; i <= 100; i++) {
+        cout << i << ". Michael Belardo" << endl;  // Replace with your name
+    }
     return 0;
 }
